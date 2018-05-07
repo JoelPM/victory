af328b2313247bd3975eb207840518e6fc29211c
Joel Meyer <joel.meyer@gmail.com>
2018-05-07 12:54:29 -0700

    Add prod.secret.exs (for now).

---
+++ config/prod.secret.exs
@@ -0,0 +1,12 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you'll likely want to automate and keep away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or yourself later on).
+config :victory, VictoryWeb.Endpoint,
+  secret_key_base: "SMhT83rAuMTOqEFxNGTSHVonJfw8BLWR6VZuVKeBclXRrBxN+JVG00oXXHUrZ969"
